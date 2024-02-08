f6fad1b84a1da454d76fdba037c7a9fb476da478
Mayorga M. F <fernanz@me.com>
2024-02-08 07:38:07 -0500

    Actualizar .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -51,3 +51,20 @@ module.exports = function(config) {
   };
 
 };
+
+// Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if(!Array.isArray(array) || array.length === 0) {
+      return [];
+    }
+    if( n < 0 ) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  // Return the smallest number argument
+  eleventyConfig.addFilter("min", (...numbers) => {
+    return Math.min.apply(null, numbers);
+  });
